5f3a0620894ec59d835dfe76e946255dfbb26f4c
Nick Yahnke <nick@pitchinteractive.com>
2020-06-23 20:11:13 -0700

    connect to obs websocket and toggle a filter

--- index.js
+++ index.js
@@ -1,32 +1,70 @@
 require('dotenv').config()
+
+const OBSWebSocket = require('obs-websocket-js');
 const tmi = require('tmi.js');
 
-const username = process.env.TWITCH_USERNAME
-const password = process.env.TWITCH_PASSWORD
-const channel = process.env.TWITCH_CHANNEL
+const twitchUsername = process.env.TWITCH_USERNAME
+const twitchPassword = process.env.TWITCH_PASSWORD
+const twitchChannel = process.env.TWITCH_CHANNEL
 
-const client = new tmi.Client({
+const obswsHost = process.env.OBS_HOST
+const obsPassword = process.env.OBS_PASSWORD
+
+
+const obs = new OBSWebSocket();
+
+const chat = new tmi.Client({
 	options: { debug: true },
 	connection: {
 		reconnect: true,
 		secure: true
 	},
 	identity: {
-		username,
-		password,
+		username: twitchUsername,
+		password: twitchPassword,
 	},
-	channels: [ channel ]
+	channels: [ twitchChannel ]
 });
-client.connect();
-client.on('message', (channel, tags, message, self) => {
-	if(self) return;
-	if(message.toLowerCase() === '!hello') {
-		client.say(channel, `@${tags.username}, heya!`);
-  }
 
-  if (message.toLowerCase().includes('mcgrath')) {
-    client.say(channel, `/timeout ${tags.username} 60`);
-    client.say(channel, `@${tags.username} shh we don't say the MCG word here`);
 
-  }
-});
+Promise.all([
+  obs.connect({ address: obswsHost, password: obsPassword }),
+  chat.connect()
+]).then(listenToChat);
+
+const filters = ['blocky']
+
+function listenToChat() {
+  console.log('listening to chat')
+  chat.on('message', (channel, tags, message, self) => {
+    if(self) return;
+    const lowercaseMessage = message.toLowerCase()
+    if(lowercaseMessage === '!hello') {
+      chat.say(channel, `@${tags.username}, heya!`);
+    }
+
+    if (lowercaseMessage.includes('mcgrath')) {
+      chat.say(channel, `/timeout ${tags.username} 60`);
+      chat.say(channel, `@${tags.username} shh we don't say the MCG word here`);
+    }
+
+    if (lowercaseMessage === '!trip') {
+      obs.send('SetSourceFilterVisibility', {
+        sourceName: 'playfield',
+        filterName: 'blocky',
+        filterEnabled: true,
+      }) // returns Promise
+    }
+    if (lowercaseMessage === '!reset') {
+      obs.send('SetSourceFilterVisibility', {
+        sourceName: 'playfield',
+        filterName: 'blocky',
+        filterEnabled: false,
+      }) // returns Promise
+    }
+
+
+
+
+  });
+}
